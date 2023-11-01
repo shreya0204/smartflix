2a787249f370df8081f4cad4a0b63a7fbdd63813
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2023-11-01 23:32:56 +0530

    ui fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
 const Header = () => {
     return (
-        <div className="absolute h-48 w-44 bg-gradient-to-b from-black">
+        <div className="absolute h-28 w-44 bg-gradient-to-b from-black z-10 top-0 left-0 flex items-center justify-center">
             <img src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-                alt="logo" />
+                alt="logo" className="w-full h-full object-contain" />
         </div>
     )
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,18 +4,17 @@ import LoginForm from '../../components/Login/LoginForm';
 const Login = () => {
 
     return (
-        <div className="relative">
+
+        <div className="relative w-screen h-screen overflow-hidden">
             <Header />
-            <div className="relative w-screen h-screen overflow-hidden">
-                <Header />
-                <img
-                    src="https://assets.nflxext.com/ffe/siteui/vlv3/dace47b4-a5cb-4368-80fe-c26f3e77d540/f5b52435-458f-498f-9d1d-ccd4f1af9913/IN-en-20231023-popsignuptwoweeks-perspective_alpha_website_small.jpg"
-                    alt="login_background_image"
-                    className="absolute w-full h-full object-cover"
-                />
-            </div>
+            <img
+                src="https://assets.nflxext.com/ffe/siteui/vlv3/dace47b4-a5cb-4368-80fe-c26f3e77d540/f5b52435-458f-498f-9d1d-ccd4f1af9913/IN-en-20231023-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+                alt="login_background_image"
+                className="absolute w-full h-full object-cover z-0"
+            />
             <LoginForm />
         </div>
+
     );
 };
 
